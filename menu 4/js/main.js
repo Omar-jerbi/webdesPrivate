6f6e68842ed8664b75413f3ce75427d4c1c1b267
Omar-jerbi <67864737+Omar-jerbi@users.noreply.github.com>
2022-03-09 11:12:58 +0100

    4 fine

--- menu 4/js/main.js
+++ menu 4/js/main.js
@@ -3,6 +3,34 @@ const menu_logo = document.querySelector('.menu-logo');
 const menu = document.querySelector('.menu');
 const body = document.querySelector('body');
 const header = document.querySelector('header');
+const dl = document.querySelector('.dl');
+
+
+const spanlight = document.querySelector('.ltag');
+const spandark = document.querySelector('.dtag');
+
+let x = 0;
+dl.addEventListener('click', () =>{
+    if(x%2 == 0){
+        body.classList.add('dark');
+        bars[0].classList.add('border-dark');
+        bars[1].classList.add('border-dark');
+        bars[2].classList.add('border-dark');
+        header.classList.add('header-border-dark');
+        spandark.classList.remove('highlight');
+        spanlight.classList.add('highlight');
+        x++;
+    }else{
+        body.classList.remove('dark');
+        bars[0].classList.remove('border-dark');
+        bars[1].classList.remove('border-dark');
+        bars[2].classList.remove('border-dark');
+        header.classList.remove('header-border-dark');
+        spandark.classList.add('highlight');
+        spanlight.classList.remove('highlight');
+        x++;
+    }
+});
 
 
 menu_logo.addEventListener('click', () => {
